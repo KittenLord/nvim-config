d0e5f3203230a528d40937f859bafee87bbb1497
Vanya <vanya.braslavets@gmail.com>
2024-03-17 00:45:33 +0200

    aaaannnnddd modified fugitive once more

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -29,7 +29,7 @@ return {
                 end, opts)
 
                 vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
+                    vim.cmd.Git({"pull"})
                 end, opts)
                 --
                 -- -- NOTE: It allows me to easily set the branch i am pushing and any tracking
