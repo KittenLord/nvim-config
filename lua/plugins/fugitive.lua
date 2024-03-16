c6ac5d2f4b19a2dc873830d3b70ac73665d2c596
Vanya <vanya.braslavets@gmail.com>
2024-03-17 00:43:44 +0200

    changed fugitive once more

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
     "tpope/vim-fugitive",
     config = function ()
         vim.keymap.set("n", "<leader>gh", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>gb", function() 
+        vim.keymap.set("n", "<leader>gb", function()
             local branch = vim.fn.input("Branch: ")
             vim.cmd.Git("branch " .. branch)
             vim.cmd.Git("checkout " .. branch)
@@ -24,14 +24,13 @@ return {
 
                 vim.keymap.set("n", "A", function() vim.cmd.Git("add .") end, opts)
 
-                -- vim.keymap.set("n", "<leader>p", function()
-                --     vim.cmd.Git('push')
-                -- end, opts)
-                --
-                -- -- rebase always
-                -- vim.keymap.set("n", "<leader>P", function()
-                --     vim.cmd.Git({'pull',  '--rebase'})
-                -- end, opts)
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git('push')
+                end, opts)
+
+                vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git({'pull',  '--rebase'})
+                end, opts)
                 --
                 -- -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- -- needed if i did not set the branch up correctly
