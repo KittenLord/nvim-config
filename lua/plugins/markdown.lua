ced0050c6c6ceb176989d44e2841218ab1310ba1
KittenLord <vanya.braslavets@gmail.com>
2025-01-11 21:12:06 +0200

    fixed markdown (hopefully)

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,24 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+    end,
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    -- https://www.reddit.com/r/neovim/comments/1b1kw8d/help_configuring_markdownpreviewnvim_with_custom/
+    -- https://github.com/iamcco/markdown-preview.nvim/issues/148
+    config = function ()
+        -- vim.cmd([[do FileType]])
+        vim.cmd([[
+            function OpenMarkdownPreview (url)
+                let cmd = "firefox --new-window " . shellescape(a:url) . " &"
+                silent call system(cmd)
+            endfunction
+            let g:mkdp_echo_preview_url = 1
+            let g:mkdp_browserfunc = 'OpenMarkdownPreview'
+        ]])
+        -- vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+        -- vim.g.mkdp_port = 8080
+    end
 }
